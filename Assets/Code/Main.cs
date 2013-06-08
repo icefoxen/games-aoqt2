9199b537c59cda3c6a632b8c6ad4a283befd6226
icefox <none@none>
2013-06-08 11:11:59 -0700

    Yay, a basic scene with some sort of background!
    Okay, what next...

--- Assets/Code/Main.cs
+++ Assets/Code/Main.cs
@@ -5,11 +5,11 @@ public class Main : MonoBehaviour {
 
 	// Use this for initialization
 	void Start() {
+		Debug.Log("Hello World once!!");
 	
 	}
 	
 	// Update is called once per frame
 	void Update() {
-		Debug.Log("Hello World!");
 	}
 }
