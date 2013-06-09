483784e8430c5cd882284828bf7c5d2e35ab4a4c
icefox <none@none>
2013-06-08 18:03:46 -0700

    Okay, I'm sort of starting to remember how to do stuff...
    Now, where shall we start?
    
    One place to start would be input and control.
    
    Another place to start would be with characters and combat model.
    
    Another place to start would be with timing and events.
    
    I think that I will start by defining an arena, with background, edges,
    and some sort of terrain map (movable vs. immovable areas).
    
    Then I will define characters to go in it, with teams, positions, basic
    attacks, hit points, and death.
    
    Then I will have to have some sort of input.
    
    Then I will make timing work properly.

--- Assets/Code/Main.cs
+++ Assets/Code/Main.cs
@@ -3,13 +3,28 @@ using System.Collections;
 
 public class Main : MonoBehaviour {
 
+	public GameObject prefabSprite;
+	
+	GameObject testsprite;
+	GameObject testsprite2;
 	// Use this for initialization
-	void Start() {
-		Debug.Log("Hello World once!!");
+	void Start ()
+	{
+		Debug.Log ("Hello World once!!");
+		testsprite = (GameObject) Instantiate (prefabSprite);
+		testsprite2 = (GameObject)Instantiate (prefabSprite);
 	
 	}
 	
 	// Update is called once per frame
-	void Update() {
+	void FixedUpdate ()
+	{
+		testsprite.transform.Translate (0.05f, 0f, 0f);
+		
+	}
+	
+	void Update()
+	{
+		testsprite2.transform.Translate (0.05f, 0f, 0f);
 	}
 }
