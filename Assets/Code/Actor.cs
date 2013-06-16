c41cdf1279a30bda3177f27915da96d2a0a02f2f
icefox <none@none>
2013-06-15 22:57:41 -0700

    Starting to figure out how it'll be shaped.

---
+++ Assets/Code/Actor.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using System.Collections;
+
+public class Actor {
+	GameObject gobj;
+	public Actor() {
+		gobj = new GameObject();
+		
+	}
+}
---
+++ Assets/Code/Battlefield.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.Collections;
+
+public class Battlefield : MonoBehaviour {
+	
+	GameObject background;
+	
+	Actor[] goodGuys;
+	Actor[] badGuys;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
