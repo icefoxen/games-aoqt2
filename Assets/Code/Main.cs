f81f1abc13e1aab60bfd76a6a6335c874a608ab1
icefox <none@none>
2013-12-06 11:37:02 -0800

    I got sort of distracted by fractals.

--- Assets/Code/Main.cs
+++ Assets/Code/Main.cs
@@ -18,7 +18,37 @@ public class Main : MonoBehaviour {
 		// So we have to init the world here, because it makes a bunch of
 		// zones which all contain background meshes and such...
 		//background.SetBackground (w);
-		player = (GameObject) Instantiate(prefabActor);
+		player = (GameObject)Instantiate (prefabActor);
+
+		Vector2 pos = Vector2.zero;
+		for (int i = 0; i < 1000; i++) {
+			Vector2 dest = Sirpenski();
+			float x = (pos.x + dest.x) / 2;
+			float y = (pos.y + dest.y) / 2;
+			pos.x = x;
+			pos.y = y;
+			var g = (GameObject) Instantiate (prefabActor);
+			g.transform.Translate(x, y, 0);
+
+
+		}
+	}
+
+	const int x1 = -15;
+	const int y1 = -15;
+	const int x2 = 15;
+	const int y2 = -15;
+	const int x3 = 0;
+	const int y3 = 15;
+	Vector2 Sirpenski() {
+		var r = Math.Floor (UnityEngine.Random.value * 3);
+		if (r == 0) {
+			return new Vector2(x1, y1);
+		} else if (r == 1) {
+			return new Vector2(x2, y2);
+		} else {
+			return new Vector2(x3, y3);
+		}
 	}
 	
 	// Update is called once per frame
--- Assets/Code/Zone.cs
+++ Assets/Code/Zone.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 
 
@@ -12,141 +13,176 @@ public struct Rect {
 
 public class Zone : MonoBehaviour {
 
-		const int WIDTH = 16;
-		const int HEIGHT = 12;
-		const int NUMMOBS = 16;
-		const int NUMPOWERUPS = 16;
+	const int WIDTH = 16;
+	const int HEIGHT = 12;
+	const int NUMMOBS = 16;
+	const int NUMPOWERUPS = 16;
 		
-		public int[, ] tiles = new int[WIDTH, HEIGHT];
-		public Mob[] mobs = new Mob[NUMMOBS];
-		public Powerup[] powerups = new Powerup[NUMPOWERUPS];
+	// XXX: I am conflicted a little with regards to terrain size and how to do
+	// the collision map for a level.
+	// FOR NOW, we will remain true to the original AOQT:
+	// Sprite maps are 16x16 sprites, for 256 per map.
+	// For the terrain map, the lower 128 sprites do not collide, the upper 128 do.
+	public int[,] tiles = new int[WIDTH, HEIGHT];
+	public Mob[] mobs = new Mob[NUMMOBS];
+	public Powerup[] powerups = new Powerup[NUMPOWERUPS];
+
+	public List<GameObject> colliders = new List<GameObject> ();
 		
-		//public const int TILEX = 4;
+	//public const int TILEX = 4;
+
+	public Rect? IntersectingTile(GameObject g) 
+	{
+		return null;
+	}
 		
-		public Rect? IntersectingTile(GameObject g) 
-		{
-			return null;
-		}
-		
-		//public void CheckTerrainCollision(
-		
-		
-		public void SetupTiles ()
-		{
-			for(int i = 0; i < WIDTH; i++) {
-				for(int j = 0; j < HEIGHT; j++) {
-					var r = (int) (Random.value * Atlas.width * Atlas.height);
-					tiles[i, j] = r;
+	//public void CheckTerrainCollision(
+
+	public bool tileIsPassable(int x, int y)
+	{
+		return tiles [x, y] < 128;
+	}
+
+	public void GenerateEmptyMap()
+	{
+		var t = 0;
+		for(int i = 0; i < WIDTH; i++) {
+			for(int j = 0; j < HEIGHT; j++) {
+				if(i == 0 || i == (WIDTH-1) || j == 0 || j == (HEIGHT-1)) {
+					tiles[i,j] = 128;
+				} else {
+					tiles[i, j] = 0;
 				}
+				tiles[i,j] = t;
+				t = (t + 1) % 256;
 			}
 		}
+	}
+
 		
-		
+	public void SetupTiles ()
+	{
 		/*
+		for(int i = 0; i < WIDTH; i++) {
+			for(int j = 0; j < HEIGHT; j++) {
+				var r = (int) (Random.value * Atlas.width * Atlas.height);
+				tiles[i, j] = r;
+			}
+		}
+		*/
+		GenerateEmptyMap ();
+	}
+
+	public void SetupColliders() 
+	{
+		// For now we just create a collider for each tile that is 
+	}
+
+		
+	/*
 	public Vector2 TileToOffset(int i) {
-		const int TILESPERROW = 2;
-		const int TILESPERCOLUMN = 2;
+	const int TILESPERROW = 2;
+	const int TILESPERCOLUMN = 2;
+
+	const float spriteW = 1.0f / TILESPERROW;
+	const float spriteH = 1.0f / TILESPERCOLUMN;
+
+	var tileXOffset = i % TILESPERROW;
+	var tileYOffset = i / TILESPERROW;
+	return new Vector2(tileXOffset, tileYOffset);
 		
-		const float spriteW = 1.0f / TILESPERROW;
-		const float spriteH = 1.0f / TILESPERCOLUMN;
-		
-		var tileXOffset = i % TILESPERROW;
-		var tileYOffset = i / TILESPERROW;
-		return new Vector2(tileXOffset, tileYOffset);
-		
-		var tileX = 0;
-		var tileY = 0;
-		
+	var tileX = 0;
+	var tileY = 0;
+
 	}
 	*/
 		
 		
-		public void InitMesh ()
-		{
-			// ...why am I doing this the hard way, anyway?
-			var mesh = new Mesh ();
-			
-			const int numSquares = WIDTH * HEIGHT;
-			var numVerts = numSquares * 4;
-			var numTris = numSquares * 6;
-			var numNormals = numSquares * 4;
-			var numUvs = numSquares * 4;
-			
-			var verts = new Vector3[numVerts];
-			var vert = 0;
-			for (int i = 0; i < WIDTH; i++) {
-				for (int j = 0; j < HEIGHT; j++) {
-					//var vert = j * WIDTH + i;
-					// All tiles have a size of 1
-					verts [vert + 0] = new Vector3 (i, j, 0);
-					verts [vert + 1] = new Vector3 ((i + 1), j, 0);
-					verts [vert + 2] = new Vector3 (i, (j + 1), 0);
-					verts [vert + 3] = new Vector3 ((i + 1), (j + 1), 0);
-					vert += 4;
-				}
-			}
-			
-			
-			mesh.vertices = verts;
-			
-			var tris = new int[numTris];
-			for (int i = 0; i < numSquares; i++) {
-				var vertOffset = i * 4;
-				var tri = i * 6;
-				tris [tri + 0] = vertOffset + 0;
-				tris [tri + 1] = vertOffset + 2;
-				tris [tri + 2] = vertOffset + 1;
-				tris [tri + 3] = vertOffset + 2;
-				tris [tri + 4] = vertOffset + 3;
-				tris [tri + 5] = vertOffset + 1;
-			}
-			
-			
-			mesh.triangles = tris;
-			
-			var normals = new Vector3[numNormals];
-			for (int i = 0; i < numNormals; i += 4) {
-				normals [i + 0] = -Vector3.forward;
-				normals [i + 1] = -Vector3.forward;
-				normals [i + 2] = -Vector3.forward;
-				normals [i + 3] = -Vector3.forward;
-			}
-			
-			
-			mesh.normals = normals;
-			
-			var uv = new Vector2[numUvs];
-			var quad = 0;
-			for(int i = 0; i < WIDTH; i++) {
-				for(int j = 0; j < HEIGHT; j++) {
-					var rect = Atlas.GetSpriteCoords(tiles[i, j]);
-					var width = rect.z;
-					var height = rect.w;
-					uv[quad + 0] = new Vector2(rect.x, rect.y);
-					uv[quad + 1] = new Vector2(rect.x + width, rect.y);
-					uv[quad + 2] = new Vector2(rect.x, rect.y + height);
-					uv[quad + 3] = new Vector2(rect.x + width, rect.y + height);
-					quad += 4;
-				}
-			}
-			
-			
-			/*
-		for (int i = 0; i < numUvs; i += 4) {
-			uv [i + 0] = new Vector2 (0, 0);
-			uv [i + 1] = new Vector2 (0.5f, 0);
-			uv [i + 2] = new Vector2 (0, 0.5f);
-			uv [i + 3] = new Vector2 (0.5f, 0.5f);
-			
-		}
-		*/
-			mesh.uv = uv;
+	public void InitMesh ()
+	{
+		// ...why am I doing this the hard way, anyway?
+		// Because it's the RIGHT WAY
+		var mesh = new Mesh ();
 
-		
-			var mf = GetComponent<MeshFilter> ();
-			mf.mesh = mesh;
+		const int numSquares = WIDTH * HEIGHT;
+		var numVerts = numSquares * 4;
+		var numTris = numSquares * 6;
+		var numNormals = numSquares * 4;
+		var numUvs = numSquares * 4;
+
+		var verts = new Vector3[numVerts];
+		var vert = 0;
+		for (int i = 0; i < WIDTH; i++) {
+			for (int j = 0; j < HEIGHT; j++) {
+				//var vert = j * WIDTH + i;
+				// All tiles have a size of 1
+				verts [vert + 0] = new Vector3 (i, j, 0);
+				verts [vert + 1] = new Vector3 ((i + 1), j, 0);
+				verts [vert + 2] = new Vector3 (i, (j + 1), 0);
+				verts [vert + 3] = new Vector3 ((i + 1), (j + 1), 0);
+				vert += 4;
+			}
 		}
 
+
+		mesh.vertices = verts;
+
+		var tris = new int[numTris];
+		for (int i = 0; i < numSquares; i++) {
+			var vertOffset = i * 4;
+			var tri = i * 6;
+			tris [tri + 0] = vertOffset + 0;
+			tris [tri + 1] = vertOffset + 2;
+			tris [tri + 2] = vertOffset + 1;
+			tris [tri + 3] = vertOffset + 2;
+			tris [tri + 4] = vertOffset + 3;
+			tris [tri + 5] = vertOffset + 1;
+		}
+
+
+		mesh.triangles = tris;
+
+		var normals = new Vector3[numNormals];
+		for (int i = 0; i < numNormals; i += 4) {
+			normals [i + 0] = -Vector3.forward;
+			normals [i + 1] = -Vector3.forward;
+			normals [i + 2] = -Vector3.forward;
+			normals [i + 3] = -Vector3.forward;
+		}
+
+
+		mesh.normals = normals;
+
+		var uv = new Vector2[numUvs];
+		var quad = 0;
+		for(int i = 0; i < WIDTH; i++) {
+			for(int j = 0; j < HEIGHT; j++) {
+				var rect = Atlas.GetSpriteCoords(tiles[i, j]);
+				var width = rect.z;
+				var height = rect.w;
+				uv[quad + 0] = new Vector2(rect.x, rect.y);
+				uv[quad + 1] = new Vector2(rect.x + width, rect.y);
+				uv[quad + 2] = new Vector2(rect.x, rect.y + height);
+				uv[quad + 3] = new Vector2(rect.x + width, rect.y + height);
+				quad += 4;
+			}
+		}
+
+
+		/*
+	for (int i = 0; i < numUvs; i += 4) {
+		uv [i + 0] = new Vector2 (0, 0);
+		uv [i + 1] = new Vector2 (0.5f, 0);
+		uv [i + 2] = new Vector2 (0, 0.5f);
+		uv [i + 3] = new Vector2 (0.5f, 0.5f);
+
+	}
+	*/
+		mesh.uv = uv;
+
+		var mf = GetComponent<MeshFilter> ();
+		mf.mesh = mesh;
+	}
 	// Use this for initialization
 	void Start () {
 		//Debug.Log ("Making zone");
